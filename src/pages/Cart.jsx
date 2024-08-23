f38c1fd75734f7c3a5c923bfff3b4984a5217405
Dash-02 <dasha.semivolos@gmail.com>
2024-08-23 22:37:43 +0300

    pizzas-work

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -127,11 +127,8 @@ function Cart() {
       }
     });
     console.log("cart ", items);
-    // const piz = items.map((it, index) => {
-    //   return Object.values(items[index]);
-    // });
-    const piz1 = Object.values(items[0]);
-    dispatch(setPizzaCart(piz1));
+
+    dispatch(setPizzaCart(items));
   }, [pizzas]);
 
   return (
@@ -175,35 +172,116 @@ function Cart() {
             <span>Очистить корзину</span>
           </div>
         </div>
-        {/* <div class="content__items">
-          {piz.map((element) => {
-            return (
-              <div class="cart__item" key={element.index}>
-                <p>
-                  {element.type}, {element.size} см.
-                </p>
-              </div>
-            );
-          })}
-        </div> */}
 
         <div class="content__items">
-          {pizzasCart.map((el) => {
-            if (el.index !== null && el.price > 0) {
-              // element.map((el) => {
+          {pizzasCart.map((element) => {
+            if (
+              Object.keys(element).length > 0 &&
+              element.index !== null &&
+              typeof Object.keys(element) === "object" &&
+              element.counter == null
+            ) {
+              console.log("l1", element);
+              return Object.values(element).map((el) => {
+                if (el.counter > 0) {
+                  return (
+                    <div class="cart__item" key={el.index}>
+                      <div class="cart__item-img">
+                        <img
+                          class="pizza-block__image"
+                          src={el.imageUrl}
+                          alt="Pizza"
+                        />
+                      </div>
+                      <div class="cart__item-info">
+                        <h3>{el.title}</h3>
+                        <p>
+                          {el.type}, {el.size} см.
+                        </p>
+                      </div>
+                      <div class="cart__item-count">
+                        <div class="button button--outline button--circle cart__item-count-minus">
+                          <svg
+                            width="10"
+                            height="10"
+                            viewBox="0 0 10 10"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
+                              fill="#EB5A1E"
+                            ></path>
+                            <path
+                              d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
+                              fill="#EB5A1E"
+                            ></path>
+                          </svg>
+                        </div>
+                        <b>{el.counter}</b>
+                        <div
+                          // onClick={handleCountPlus(el.index)}
+                          class="button button--outline button--circle cart__item-count-plus"
+                        >
+                          <svg
+                            width="10"
+                            height="10"
+                            viewBox="0 0 10 10"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
+                              fill="#EB5A1E"
+                            ></path>
+                            <path
+                              d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
+                              fill="#EB5A1E"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <div class="cart__item-price">
+                        <b>{el.price} ₽</b>
+                      </div>
+                      <div class="cart__item-remove">
+                        <div class="button button--outline button--circle">
+                          <svg
+                            width="10"
+                            height="10"
+                            viewBox="0 0 10 10"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
+                              fill="#EB5A1E"
+                            ></path>
+                            <path
+                              d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
+                              fill="#EB5A1E"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                }
+              });
+            } else if (element.counter === 1) {
               return (
-                <div class="cart__item" key={el.index}>
+                <div class="cart__item" key={element.index}>
                   <div class="cart__item-img">
                     <img
                       class="pizza-block__image"
-                      src={el.imageUrl}
+                      src={element.imageUrl}
                       alt="Pizza"
                     />
                   </div>
                   <div class="cart__item-info">
-                    <h3>{el.title}</h3>
+                    <h3>{element.title}</h3>
                     <p>
-                      {el.type}, {el.size} см.
+                      {element.type}, {element.size} см.
                     </p>
                   </div>
                   <div class="cart__item-count">
@@ -225,7 +303,7 @@ function Cart() {
                         ></path>
                       </svg>
                     </div>
-                    <b>{el.counter}</b>
+                    <b>{element.counter}</b>
                     <div
                       // onClick={handleCountPlus(el.index)}
                       class="button button--outline button--circle cart__item-count-plus"
@@ -249,7 +327,7 @@ function Cart() {
                     </div>
                   </div>
                   <div class="cart__item-price">
-                    <b>{el.price} ₽</b>
+                    <b>{element.price} ₽</b>
                   </div>
                   <div class="cart__item-remove">
                     <div class="button button--outline button--circle">
@@ -273,11 +351,9 @@ function Cart() {
                   </div>
                 </div>
               );
-              // });
             } else {
               return <></>;
             }
-            // });
           })}
         </div>
         <div class="cart__bottom">
